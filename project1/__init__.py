179e912f1e401bd5b73903cc96045c2eb388f4a1
Biswas Nandamuri <biswas.bin2@gmail.com>
2022-03-23 21:22:46 -0500

    (test) Add tests for Settings class

--- project1/__init__.py
+++ project1/__init__.py
@@ -1,1 +1,18 @@
+import spacy
 
+from project1.redactors import name_redactor
+
+BLOCK_CHARACTER = '\u2588'
+
+
+def redact_pipeline(
+        unredacted_txt: str,
+        redacts: dict[str, bool]):
+
+    nlp = spacy.load('en_core_web_md')
+    doc = nlp(unredacted_txt)
+
+    # if 'names' in redacts and redacts['names'] is True:
+    #     doc = name_redactor.redact(doc, BLOCK_CHARACTER)
+
+    return doc.text
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,16 +2,23 @@ import pytest
 from project1 import models
 
 test_cases = [
-    ({'input': ['*.csv', '*.txt'],'output': 'files/'},
-        models.Settings(input=['*.txt', '*.csv'],output='files/'),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True},
+        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': True}),
         True),
-    ({'input': ['*.pdf'],'output': 'files/'},
-        models.Settings(input=['*.txt', '*.csv'],output='files/'),
+    ({'input': ['*.pdf'], 'output': 'files/1', 'names': True},
+        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': True}),
         False),
-    ({'input': ['*.csv', '*.txt'],'output': 'dummy/'},
-        models.Settings(input=['*.txt', '*.csv'],output='files/'),
-        False)
+    ({'input': ['*.csv', '*.txt'], 'output': 'dummy/', 'names': True},
+        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': True}),
+        False),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True},
+        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': False}),
+        False),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True},
+        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': False, 'dummy': True}),
+        False),
 ]
+
 
 @pytest.mark.parametrize("input,want,result", test_cases)
 def test_settings_dunder_equal_func(input, want, result):
@@ -19,9 +26,19 @@ def test_settings_dunder_equal_func(input, want, result):
 
     assert (got == want) is result
 
+
 def test_settings_dunder_str_func():
-    temp_args = {'input': ['*.csv', '*.txt'],'output': 'files/'}
-    got = models.Settings.parse(temp_args)
-    want = 'Settings (\n\tinput: {0}\n\toutput: {1}\n)'.format(str(temp_args['input']), temp_args['output'])
+    args_in = {'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True}
+    args_out = {
+        'input': [
+            '*.csv',
+            '*.txt'],
+        'output': 'files/',
+        'redacts': {
+            'names': True}}
+
+    got = models.Settings.parse(args_in)
+    want = 'Settings (\n\tinput: {0}\n\toutput: {1}\n\tredacts: {2}\n)'.format(
+        str(args_out['input']), args_out['output'], str(args_out['redacts']))
 
     assert str(got) == want

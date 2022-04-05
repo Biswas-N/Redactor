9217092596d6fa4e0d11e15c94f63a8acc1eda6d
Biswas Nandamuri <biswas.bin2@gmail.com>
2022-04-05 10:12:44 -0500

    (feat) Add support for other finders and respective tests

--- project1/models/settings.py
+++ project1/models/settings.py
@@ -2,16 +2,21 @@ from typing import Any
 
 
 class Settings(object):
-    SUPPORTED_REDACTS: list[str] = ['names']
+    SUPPORTED_REDACTS: list[str] = [
+        'names', 'genders', 'dates', 'phones', 'address']
 
     def __init__(self,
                  input: list[str],
                  output: str,
-                 redacts: dict[str, bool]) -> None:
+                 redacts: dict[str, bool],
+                 concepts: list[str],
+                 stats: str) -> None:
 
         self.input = input
         self.output = output
         self.redacts = redacts
+        self.concepts = concepts
+        self.stats = stats
 
     def __eq__(self, __o: object) -> bool:
         # Finding if the inputs list in self and __o are equal
@@ -34,18 +39,31 @@ class Settings(object):
             if __o.redacts[i] != self.redacts[i]:
                 return False
 
+        # Checking if 'concepts' are equal
+        concepts_intersections = frozenset(
+            __o.concepts).intersection(
+            self.concepts)
+        if len(concepts_intersections) != len(self.concepts):
+            return False
+
+        # Checking if 'stats' variables are equal
+        if self.stats != __o.stats:
+            return False
+
         return True
 
     def __str__(self) -> str:
-        return f'Settings (\n\tinput: {str(self.input)}\n\toutput: {str(self.output)}\n\tredacts: {str(self.redacts)}\n)'
+        return f'Settings (\n\tinput: {str(self.input)}\n\toutput: {str(self.output)}\n\tredacts: {str(self.redacts)}\n\tconcepts: {str(self.concepts)}\n\tstats: {str(self.stats)}\n)'
 
     @staticmethod
     def parse(args: dict[str, Any]):
         redacts: dict[str, bool] = {k: v for (k, v) in args.items(
-        ) if k in Settings.SUPPORTED_REDACTS and v is True}
+        ) if k in Settings.SUPPORTED_REDACTS}
 
         return Settings(
             input=args['input'],
             output=args['output'],
-            redacts=redacts
+            redacts=redacts,
+            concepts=args['concept'],
+            stats=args['stats']
         )
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,20 +2,84 @@ import pytest
 from project1 import models
 
 test_cases = [
-    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True},
-        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': True}),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True, 'concept': ['kids', 'war'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt',
+            '*.csv'],
+        output='files/',
+        redacts={
+            'names': True},
+        concepts=['kids', 'war'],
+        stats='stdout'),
         True),
-    ({'input': ['*.pdf'], 'output': 'files/1', 'names': True},
-        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': True}),
+    ({'input': ['*.csv', '*.txt'], 'output': 'dummy/', 'names': True, 'concept': ['kids'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt'],
+        output='files/',
+        redacts={
+            'names': True},
+        concepts=['kids', 'war'],
+        stats='stdout'),
         False),
-    ({'input': ['*.csv', '*.txt'], 'output': 'dummy/', 'names': True},
-        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': True}),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True, 'concept': ['kids'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt',
+            '*.csv'],
+        output='dummy/',
+        redacts={
+            'names': True},
+        concepts=['kids', 'war'],
+        stats='stdout'),
         False),
-    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True},
-        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': False}),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True, 'concept': ['kids'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt',
+            '*.csv'],
+        output='files/',
+        redacts={
+            'names': True,
+            'genders': False},
+        concepts=['kids', 'war'],
+        stats='stdout'),
         False),
-    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True},
-        models.Settings(input=['*.txt', '*.csv'], output='files/', redacts={'names': False, 'dummy': True}),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True, 'genders': True, 'concept': ['kids'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt',
+            '*.csv'],
+        output='files/',
+        redacts={
+            'names': True,
+            'genders': False},
+        concepts=['kids'],
+        stats='stdout'),
+        False),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True, 'genders': True, 'concept': ['kids', 'dog'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt',
+            '*.csv'],
+        output='files/',
+        redacts={
+            'names': True,
+            'genders': True},
+        concepts=['kids'],
+        stats='stdout'),
+        False),
+    ({'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True, 'concept': ['kids', 'war'], 'stats': 'stdout'},
+        models.Settings(
+        input=[
+            '*.txt',
+            '*.csv'],
+        output='files/',
+        redacts={
+            'names': True},
+        concepts=['kids', 'war'],
+        stats='dummy'),
         False),
 ]
 
@@ -28,17 +92,30 @@ def test_settings_dunder_equal_func(input, want, result):
 
 
 def test_settings_dunder_str_func():
-    args_in = {'input': ['*.csv', '*.txt'], 'output': 'files/', 'names': True}
+    args_in = {
+        'input': [
+            '*.csv',
+            '*.txt'],
+        'output': 'files/',
+        'names': True,
+        'concept': [
+            'kids',
+            'war'],
+        'stats': "stdout"}
     args_out = {
         'input': [
             '*.csv',
             '*.txt'],
         'output': 'files/',
         'redacts': {
-            'names': True}}
+            'names': True},
+        'concepts': [
+            'kids',
+            'war'],
+        'stats': "stdout"}
 
     got = models.Settings.parse(args_in)
-    want = 'Settings (\n\tinput: {0}\n\toutput: {1}\n\tredacts: {2}\n)'.format(
-        str(args_out['input']), args_out['output'], str(args_out['redacts']))
+    want = 'Settings (\n\tinput: {0}\n\toutput: {1}\n\tredacts: {2}\n\tconcepts: {3}\n\tstats: {4}\n)'.format(
+        str(args_out['input']), args_out['output'], str(args_out['redacts']), str(args_out['concepts']), args_out['stats'])
 
     assert str(got) == want

9dce1d79a49983339d0722530fa5ae7374f1b540
Biswas Nandamuri <biswas.bin2@gmail.com>
2022-04-05 10:14:01 -0500

    (feat) Add stdout, stderr and file support for stats and output

--- project1/__init__.py
+++ project1/__init__.py
@@ -1,19 +1,27 @@
-import spacy
-from spacy.tokens import Doc, Span
+from project1 import entity_finders
+from typing import Tuple
 import re
+import spacy
+import en_core_web_md
+from spacy.tokens import Doc, Span
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator
+import nltk
+nltk.download("wordnet", quiet=True)
+nltk.download("omw", quiet=True)
 
-from project1.entity_finders import names_finder
 
 BLOCK_CHARACTER = '\u2588'
 
 
 def redact_pipeline(
         unredacted_txt: str,
-        redacts: dict[str, bool]) -> str:
+        redacts: dict[str, bool],
+        concepts: list[str]) -> Tuple[str, str]:
 
     unredacted_txt = re.sub(r' +', ' ', unredacted_txt)
 
-    nlp = spacy.load("en_core_web_md")
+    nlp = en_core_web_md.load()
+
     doc = nlp(unredacted_txt)
     with doc.retokenize() as retokenizer:
         for ent in doc.ents:
@@ -21,9 +29,36 @@ def redact_pipeline(
 
     redactions: list[Span] = []
     if 'names' in redacts and redacts['names'] is True:
-        redactions += names_finder(doc, nlp=nlp)
+        redactions += entity_finders.names_finder(doc, nlp=nlp)
 
-    return redact(doc, redactions)
+    if 'genders' in redacts and redacts['genders'] is True:
+        redactions += entity_finders.genders_finder(doc, nlp=nlp)
+
+    if 'dates' in redacts and redacts['dates'] is True:
+        redactions += entity_finders.dates_finder(doc, nlp=nlp)
+
+    if 'phones' in redacts and redacts['phones'] is True:
+        redactions += entity_finders.phones_finder(doc, nlp=nlp)
+
+    if 'address' in redacts and redacts['address'] is True:
+        redactions += entity_finders.address_finder(doc, nlp=nlp)
+
+    if len(concepts) > 0:
+        nlp = spacy.load("en_core_web_md")
+        nlp.add_pipe(
+            "spacy_wordnet",
+            after='tagger',
+            config={
+                'lang': nlp.lang})
+
+        doc = nlp(unredacted_txt)
+        with doc.retokenize() as retokenizer:
+            for ent in doc.ents:
+                retokenizer.merge(ent)
+        redactions += entity_finders.concept_finder(
+            doc, nlp=nlp, concepts=concepts)
+
+    return redact(doc, redactions), get_stats(doc, redactions)
 
 
 def redact(doc: Doc, redactions: list[Span]) -> str:
@@ -33,3 +68,27 @@ def redact(doc: Doc, redactions: list[Span]) -> str:
             redacted_text[ent.start_char: ent.end_char]) + redacted_text[ent.end_char:]
 
     return redacted_text
+
+
+def get_stats(doc: Doc, redactions: list[Span]) -> str:
+    stats_txt_arr = []
+
+    redact_types = [
+        "mNAME",
+        "mGENDER",
+        "mDATE",
+        "mPHONENUM",
+        "mADDRESS",
+        "mCONCEPT"]
+
+    for redact_type in redact_types:
+        redacts_for_type = [(r.start_char, r.end_char)
+                            for r in redactions if r.label_ == redact_type]
+        stats_txt_arr.append(
+            f"{redact_type[1:]}: {len(redacts_for_type)} redactions")
+
+        for start, end in sorted(redacts_for_type):
+            stats_txt_arr.append(
+                f"\tStart: {start}, End: {end} => {doc.text[start: end]}")
+
+    return "\n".join(stats_txt_arr)
--- redactor.py
+++ redactor.py
@@ -16,71 +16,119 @@ def main(settings: Settings):
 
     print('\nFiles processing ->', input_files, '\n')
 
-    if settings.output == 'stdout' or settings.output == 'stderr':
-        output_to_std(input_files, settings.output)        
-    else:
-        # Creating output path irrespective of OS (Windows and Posix)
-        output_path = Path(os.path.join(os.getcwd(), settings.output)).resolve()
+    process_stats = []
+    for input_file in input_files:
+        redacted_txt = ""
 
-        for raw_file in input_files:
-            input_file = Path(raw_file).resolve()
-            output_file_folder = Path(os.path.join(output_path, raw_file)).parent
-            Path(output_file_folder).resolve().mkdir(parents=True, exist_ok=True)
-            output_file = os.path.join(
-                output_file_folder,
-                f'{Path(raw_file).stem}.redacted')
-
-            print(f'Redacting {raw_file} -> {output_file}')
-
-            with open(input_file, 'r') as in_f, open(output_file, 'w') as out_f:
+        input_file_path = Path(input_file).resolve()
+        try:
+            with open(input_file_path, 'r') as in_f:
                 unredacted_txt = in_f.read()
-                redacted_txt = redact_pipeline(unredacted_txt, settings.redacts)
+                redacted_txt, stats = redact_pipeline(
+                    unredacted_txt,
+                    redacts=settings.redacts,
+                    concepts=settings.concepts)
 
-                out_f.write(redacted_txt)
+                process_stats.append(
+                    f"========== Stats:{input_file} ==========\n{stats}")
+
+        except BaseException:
+            sys.stderr.write(f'Could not read and redact {input_file}\n\n')
+            continue
+
+        if settings.output == 'stdout' or settings.output == 'stderr':
+            print(f'Redacting {input_file} ->')
+            output_to_std(redacted_txt, settings.output)
+        else:
+            # Creating output path irrespective of OS (Windows and Posix)
+            output_file_folder = Path(
+                os.path.join(
+                    settings.output,
+                    input_file)).parent
+            output_file_folder.resolve().mkdir(parents=True, exist_ok=True)
+            output_file = Path(os.path.join(
+                output_file_folder,
+                f'{Path(input_file).stem}.redacted'))
+
+            print(f'Redacting {input_file} -> {output_file.resolve()}')
+            output_to_file(redacted_txt, output_file)
+
+        print(f'Stats added to {settings.stats}\n')
+
+    if settings.stats == 'stdout' or settings.stats == 'stderr':
+        output_to_std("\n\n".join(process_stats), settings.stats)
+    else:
+        output_file = Path(os.path.join(os.getcwd(), settings.stats))
+        output_to_file("\n\n".join(process_stats), output_file)
 
 
-def output_to_std(input_files: list, output: str):
+def output_to_std(content: str, output: str):
     old_stdout_state = sys.stdout
     if output == 'stderr':
         sys.stdout = sys.stderr
-    
-    for raw_file in input_files:
-        input_file = Path(raw_file).resolve()
 
-        print(f'Redacting {raw_file}...\n')
+    sys.stdout.write(content)
+    sys.stdout.write('\n\n')
 
-        with open(input_file, 'r') as in_f:
-            unredacted_txt = in_f.read()
-            redacted_txt = redact_pipeline(unredacted_txt, settings.redacts)
-
-            sys.stdout.write(redacted_txt)
-            sys.stdout.write('\n')
-            
     sys.stdout = old_stdout_state
+
+
+def output_to_file(content: str, output_file: Path):
+    with open(output_file.resolve(), 'w', encoding='utf-8') as out_f:
+        out_f.write(content)
+
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
-        description='Redacts sensitive content based on given flags.')
+        description='Redacts sensitive content in a given file.')
     arg_parser.add_argument(
         "--input",
         required=True,
         action="append",
-        help="glob of input files (eg:'*.txt')")
+        help="<Required> glob of input files (eg:'*.txt')")
     arg_parser.add_argument(
         "--output",
         required=True,
-        help="directory to store redacted files")
+        help="<Required> directory to store redacted files")
     arg_parser.add_argument(
         "--names",
         action="store_true",
-        help="redacts any type of names (Human, Location and Organizations)")
-    # arg_parser.add_argument(
-    #     "--stats",
-    #     required=True,
-    #     help="redacts any type of names")
+        default=False,
+        help="redacts names (Human, Location and Organizations)")
+    arg_parser.add_argument(
+        "--genders",
+        action="store_true",
+        default=False,
+        help="redacts gender revealing words (He, She, Father, Mother)")
+    arg_parser.add_argument(
+        "--dates",
+        action="store_true",
+        default=False,
+        help="redacts dates (Feb 3rd, 03/02/2022, February 3, 2022)")
+    arg_parser.add_argument(
+        "--phones",
+        action="store_true",
+        default=False,
+        help="redacts phone numbers (only 10 digit phone numbers)")
+    arg_parser.add_argument(
+        "--address",
+        action="store_true",
+        default=False,
+        help="redacts US based physical addresses")
+    arg_parser.add_argument(
+        "--concept",
+        action='append',
+        help='<Required> redacts sentence based on given concept',
+        required=True)
+    arg_parser.add_argument(
+        "--stats",
+        required=True,
+        help="<Required> mode to show (stdout, stderr) or save redacted files")
 
     args = arg_parser.parse_args()
     settings = Settings.parse(vars(args))
-    print(settings)
 
-    main(settings)
+    try:
+        main(settings)
+    except Exception as e:
+        sys.stderr.write("Unexpected error occured!\n")
